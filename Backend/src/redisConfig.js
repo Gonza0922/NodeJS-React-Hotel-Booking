d74a245a22de8ce6c5be7277217eeafec934deb3
Gonzalo <gonzalo.nieto0922@gmail.com>
2024-08-17 15:51:38 -0300

    backend-feat: añadimos redis en el docker-compose.yml, utilizando tambien, los entornos de desarrollo para conectarse desde el back

--- Backend/src/redisConfig.js
+++ Backend/src/redisConfig.js
@@ -1,12 +1,25 @@
 import redis from "redis";
 
+const { NODE_ENV, REDIS_HOST, DOCKER_REDIS_HOST } = process.env;
+const nodeEnv = NODE_ENV && NODE_ENV.trim().toLowerCase();
+let host = REDIS_HOST;
+
+if (nodeEnv === "production") host = DOCKER_REDIS_HOST;
+
 const redisClient = redis.createClient({
-  host: "127.0.0.1",
-  port: 6379,
+  socket: {
+    host,
+    port: 6379,
+  },
 });
 
-redisClient.on("error", (err) => console.log("Redis Client Error", err));
-
-await redisClient.connect();
+redisClient
+  .connect()
+  .then(() => {
+    console.log(`Connected to Redis successfully with host ${host}`);
+  })
+  .catch((err) => {
+    console.error("Failed to connect to Redis:", err);
+  });
 
 export default redisClient;
