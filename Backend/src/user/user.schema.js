27ef9c106ff2dcc789733b7d20af468d0e1588ff
Gonzalo <gonzalo.nieto0922@gmail.com>
2024-10-17 20:06:15 -0300

    backend-fix: al hacer login no se verifica que la password debe tener al menos 6 caracteres

--- Backend/src/user/user.schema.js
+++ Backend/src/user/user.schema.js
@@ -38,9 +38,7 @@ export const login = z.object({
   email: z
     .string({ required_error: "Email is required" })
     .email({ message: "Email is not valid" }),
-  password: z
-    .string({ required_error: "Password is required" })
-    .min(6, { message: "Password must be at least 6 characters" }),
+  password: z.string({ required_error: "Password is required" }),
 });
 
 export const update = z.object({
