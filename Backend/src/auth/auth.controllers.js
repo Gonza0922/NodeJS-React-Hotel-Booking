f7c862cd581bc7aad578ae8affd2bc2ccb81d55b
Gonzalo <gonzalo.nieto0922@gmail.com>
2024-11-09 14:31:25 -0300

    backend-refactor: utilizamos el role para saber si autenticamos un user o un partner

--- Backend/src/auth/auth.controllers.js
+++ Backend/src/auth/auth.controllers.js
@@ -4,11 +4,15 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res) => {
-  //Register a new user
+  //Register a new user/partner
   try {
+    const { role } = req.body;
+    if (role !== "users" && role !== "partners")
+      return res.status(400).json({ message: "Invalid role" });
+    const roleId = role === "users" ? "user" : "partner";
+    const stringRole = role === "users" ? "User" : "Partner";
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    const q =
-      "INSERT INTO users(email, password, first_name, last_name, birthdate, nacionality, phone) VALUES (?)";
+    const q = `INSERT INTO ${role}(email, password, first_name, last_name, birthdate, nacionality, phone) VALUES (?)`;
     const values = [
       req.body.email,
       hashedPassword,
@@ -18,73 +22,97 @@ export const registerUser = async (req, res) => {
       req.body.nacionality,
       req.body.phone,
     ];
-    const [findEmail] = await db.query("SELECT email FROM users WHERE email = ?", [
+    const [findEmail] = await db.query(`SELECT email FROM ${role} WHERE email = ?`, [
       req.body.email,
     ]);
     if (findEmail.length > 0) return res.status(409).json({ message: ["Email already exists"] });
     const createUser = await db.query(q, [values]);
     const user_ID = createUser[0].insertId;
-    const [user] = await db.query("SELECT * FROM users WHERE user_ID = ?", [user_ID]);
-    const token = await generateToken({ user_ID: user[0].user_ID });
-    res.cookie("UserToken", token);
-    res.status(201).json(user[0]);
+    const [user] = await db.query(`SELECT * FROM ${role} WHERE ${roleId}_ID = ?`, [user_ID]);
+    const tokenPayload =
+      role === "users"
+        ? { user_ID: user[0].user_ID, role }
+        : { partner_ID: user[0].partner_ID, role };
+    const token = await generateToken(tokenPayload);
+    res.cookie(`${stringRole}Token`, token);
+    res.status(201).json(user[0], token);
   } catch (err) {
     console.error("Error:", err);
-    res.status(500).json({ error: "Failed to register User" });
+    res.status(500).json({ error: "Failed to register User/Partner" });
   }
 };
 
 export const loginUser = async (req, res) => {
-  //Log in a user that matches the data sent
+  //Log in a user/partner that matches the data sent
   try {
-    const { email, password } = req.body;
-    const [user] = await db.query("SELECT * FROM users WHERE email = ?", [email]);
-    if (user.length === 0) return res.status(404).json({ message: "User not found" });
+    const { email, password, role } = req.body;
+    if (role !== "users" && role !== "partners")
+      return res.status(400).json({ message: "Invalid role" });
+    const stringRole = role === "users" ? "User" : "Partner";
+    const [user] = await db.query(`SELECT * FROM ${role} WHERE email = ?`, [email]);
+    if (user.length === 0) return res.status(404).json({ message: `${stringRole} not found` });
     const isMatch = await bcrypt.compare(password, user[0].password);
     if (!isMatch) return res.status(401).json({ message: "Incorrect Password" });
-    const token = await generateToken({ user_ID: user[0].user_ID });
-    res.cookie("UserToken", token);
-    res.status(200).json({
-      user_ID: user[0].user_ID,
+    const tokenPayload =
+      role === "users"
+        ? { user_ID: user[0].user_ID, role }
+        : { partner_ID: user[0].partner_ID, role };
+    const token = await generateToken(tokenPayload);
+    res.cookie(`${stringRole}Token`, token);
+    const responseData = {
+      message: "Logged in",
       first_name: user[0].first_name,
       last_name: user[0].last_name,
-      message: "Logged in",
-    });
+      ...(role === "users" ? { user_ID: user[0].user_ID } : { partner_ID: user[0].partner_ID }),
+    };
+    res.status(200).json(responseData);
   } catch (err) {
     console.error("Error:", err);
-    res.status(500).json({ error: "Failed to login User" });
+    res.status(500).json({ error: "Failed to login User/Partner" });
   }
 };
 
 export const logoutUser = async (req, res) => {
-  //Log out a partner
+  //Log out a user/partner
   try {
-    res.cookie("UserToken", "", { expires: new Date(0) });
+    const { role } = req.body;
+    const stringRole = role === "users" ? "User" : "Partner";
+    res.cookie(`${stringRole}Token`, "", { expires: new Date(0) });
     return res.status(200).json({ message: "Disconnected" });
   } catch (err) {
     console.error("Error:", err);
-    res.status(500).json({ error: "Failed to logout user" });
+    res.status(500).json({ error: "Failed to logout User/Partner" });
   }
 };
 
 export const verifyUser = async (req, res) => {
-  //Check if the UserToken exists/matches to enter the user account
+  //Check if the UserToken/PartnerToken exists/matches to enter the user/partner account
   try {
-    const { UserToken } = req.cookies;
-    if (!UserToken) return res.status(401).json({ message: "Unauthorized, no token" });
-    jwt.verify(UserToken, process.env.TOKEN_SECURE, async (err, user) => {
+    const { UserToken, PartnerToken } = req.cookies;
+    const token = UserToken || PartnerToken;
+    if (!token) return res.status(401).json({ message: "Unauthorized, no token" });
+    jwt.verify(token, process.env.TOKEN_SECURE, async (err, user) => {
       if (err) return res.status(400).json({ message: "Verification error" });
-      const [userFound] = await db.query("SELECT * FROM users WHERE user_ID = ?", [user.user_ID]);
-      if (userFound.length === 0) return res.status(404).json({ message: "User not found" });
-      return res.status(200).json({
-        user_ID: userFound[0].user_ID,
+      const { role } = user;
+      const roleId = role === "users" ? "user" : "partner";
+      const stringRole = role === "users" ? "User" : "Partner";
+      const [userFound] = await db.query(`SELECT * FROM ${role} WHERE ${roleId}_ID = ?`, [
+        user.user_ID,
+      ]);
+      if (userFound.length === 0)
+        return res.status(404).json({ message: `${stringRole} not found` });
+      const responseData = {
         first_name: userFound[0].first_name,
         last_name: userFound[0].last_name,
         email: userFound[0].email,
-      });
+        ...(role === "users"
+          ? { user_ID: userFound[0].user_ID }
+          : { partner_ID: userFound[0].partner_ID }),
+      };
+      return res.status(200).json(responseData);
     });
   } catch (err) {
     console.error("Error:", err);
-    res.status(500).json({ error: "Failed to verify User" });
+    res.status(500).json({ error: "Failed to verify User/Partner" });
   }
 };
